b86fb03a77f1693a1c0a74a28e9b330a4d17184a
nicolas.fanin <nicolas.x.fanin.-nd@disney.com>
2019-08-24 17:24:22 -0300

    Search bar added

--- app/src/main/java/com/nicolasfanin/userapp/ui/fragments/ProfileSearchFragment.kt
+++ app/src/main/java/com/nicolasfanin/userapp/ui/fragments/ProfileSearchFragment.kt
@@ -2,10 +2,13 @@ package com.nicolasfanin.userapp.ui.fragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.support.v4.view.MenuItemCompat
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.SearchView
+import android.support.v7.widget.Toolbar
+import android.view.*
 import com.nicolasfanin.userapp.R
+import com.nicolasfanin.userapp.ui.activities.MainActivity
 
 class ProfileSearchFragment : Fragment() {
 
@@ -16,6 +19,24 @@ class ProfileSearchFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_profile_search, container, false)
+        var view = inflater.inflate(R.layout.fragment_profile_search, container, false)
+
+        var searchToolbar = view.findViewById<Toolbar>(R.id.search_toolbar)
+        (activity as AppCompatActivity).setSupportActionBar(searchToolbar)
+
+
+        setHasOptionsMenu(true)
+        return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        menu!!.clear()
+        inflater?.inflate(R.menu.search_menu, menu)
+        val searchView = SearchView((context as MainActivity).supportActionBar?.themedContext ?: context)
+        menu.findItem(R.id.action_search).apply {
+            setShowAsAction(MenuItem.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW or MenuItem.SHOW_AS_ACTION_IF_ROOM)
+            actionView = searchView
+        }
     }
 }
